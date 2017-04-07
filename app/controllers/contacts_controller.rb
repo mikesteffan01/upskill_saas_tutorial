6369c92e4771c6586229c90758a39064af51db62
Mike Steffan <mikesteffan01@gmail.com>
2017-04-07 17:46:42 +0000

    Improved flash stye

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
       if @contact.save
         flash[:success] = "Message sent."
       else 
-        flash[:error] = @contact.errors.full_messages.join(", ")
+        flash[:danger] = @contact.errors.full_messages.join(", ")
         redirect_to new_contact_path
       end
     end
